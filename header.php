cade063508daa80f557fd200c9ea0b58981e857e
Thomas Seisveld <tseisveld@gmail.com>
2023-05-26 09:19:52 +0200

    v

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="Index.html">home</a></li>
+        <li><a href="">about us</a></li>
+        <li><a href="">blog</a></li>
+        <li><a href="">Sign up</a></li>
+        <li><a href="">Log in</a></li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'header.php';
+?>
+<div>
+    hoi
+</div>
+<?php
+include_once 'header.php';
+?>
